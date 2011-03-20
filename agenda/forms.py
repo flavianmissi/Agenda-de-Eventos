3c54cbb7166c96c875f6002e996224dcfc594521
Flavia Missi <flaviamissi@gmail.com>
2011-03-20 09:53:33 -0300

    Using model form

--- agenda/forms.py
+++ agenda/forms.py
@@ -1,12 +1,15 @@
 #-*- coding: utf-8 -*-
 
-from django import forms
+from django.forms import ModelForm
+from agenda.models import ItemAgenda
 
-class FormItemAgenda(forms.Form):
-    titulo = forms.CharField(max_length=100)
-    data = forms.DateField(
-        widget=forms.DateInput(format='%d/%m/%Y'),
-        input_formats=['%d/%m/%y', '%d/%m/%Y']
-    )
-    hora = forms.TimeField()
-    descricao = forms.CharField()
+class FormItemAgenda(ModelForm):
+    #titulo = forms.CharField(max_length=100)
+    #data = forms.DateField(
+    #    widget=forms.DateInput(format='%d/%m/%Y'),
+    #    input_formats=['%d/%m/%y', '%d/%m/%Y']
+    #)
+    #hora = forms.TimeField()
+    #descricao = forms.CharField()
+    class Meta:
+        model = ItemAgenda
